abbf37feb34f255838fd321fa50a8d83a405acad
InfernoX7 <Inferno.x@live.com>
2017-02-23 10:31:12 +0000

    Update index.php

--- php/index.php
+++ php/index.php
@@ -7,23 +7,23 @@
 ?>
 <body>
 	<h2>View All Members Details</h2>
-	<form action ="listMembers.php" method="post">
+	<form action ="listMember.php" method="post">
 		<input type = "submit" value = 'View Members' name = "viewMembers"><br>
 	</form>
 	
 	<h2>View All Members /w Trainer</h2>
-	<form action ="listMembers.php" method="post">
+	<form action ="listMember.php" method="post">
 		<input type = "submit" value = 'View Members & Trainers' name = "viewMemTrain"><br>
 	</form>
 	
 	<h2>Add a Trainer</h2>
-	<form action = "changeLastName.php" method ="post">
+	<form action = "changeTrainer.php" method ="post">
 		Name <input type="text" name = "newTrainer"><br>
 		<Input type = "submit" value = "Add New Trainer"><br>
 	</form>
 	
 	<h2>Delete a Trainer</h2>
-	<form action = "changeLastName.php" method ="post">
+	<form action = "changeTrainer.php" method ="post">
 		Name <input type="text" name = "delTrainer"><br>
 		<Input type = "submit" value = "Delete Trainer"><br>
 	</form>
@@ -36,17 +36,36 @@
 		<Input type = "submit" value = "Change Last Name"><br>
 	</form>
 	
-	<h2>Enroll a Member in a Class</h2>
+	<h2>Enroll a Member in a Class (Using IDs)</h2>
 	<form action = "enroll.php" method ="post">
 		Member ID <input type="text" name = "memID"><br>
 		Class ID <input type="text" name = "classID"><br>
-		<Input type = "submit" value = "Enroll"><br>
+		<Input type = "submit" value = "Enroll ID"><br>
 	</form>
 	
 	<h2>View All Members /w Class & Trainer</h2>
-	<form action ="listMembers.php" method="post">
+	<form action ="listMember.php" method="post">
 		<input type = "submit" value = 'View Member & Class' name = "viewMemClass"><br>
 	</form>
 	
+	<h2>View Payment Details</h2>
+	<form action = "listPayment.php" method ="post">
+		First Name <input type="text" name = "fName"><br>
+		Last Name <input type="text" name = "lName"><br>
+		<Input type = "submit" value = "Veiw Payments"><br>
+	</form>
 	
+	<h2>Enroll a Member in a Class (Using Names)</h2>
+	<form action = "enroll.php" method ="post">
+		First Name <input type="text" name = "fName"><br>
+		Last Name <input type="text" name = "lName"><br>
+		Class Name <input type="text" name = "className"><br>
+		<Input type = "submit" value = "Enroll Name"><br>
+	</form>
+	
+	<h2>Trainer /w Classes</h2>
+	<form action = "listTrainer.php" method ="post">
+		Name <input type="text" name = "newTrainer"><br>
+		<Input type = "submit" value = "View Trainers"><br>
+	</form>
 </body>
